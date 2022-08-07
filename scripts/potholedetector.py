a6695a4c1564d77044a3d33a5a689b9150bf1921
Harish <harishprasanna66@gmail.com>
2022-08-07 19:03:26 +0530

    Got coords from contours and Published untransformed coordinates in a pointcloud

--- scripts/potholedetector.py
+++ scripts/potholedetector.py
@@ -2,13 +2,19 @@
 
 import rospy
 import cv2
+import numpy as np
 
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import PointCloud
+from geometry_msgs.msg import Point
+from geometry_msgs.msg import Point32
+import std_msgs.msg
 
 class camera_1:
 
   def __init__(self):
+    self.cloud_pub=rospy.Publisher("point_cloud", PointCloud, queue_size=10 )
     self.image_sub = rospy.Subscriber("virat/camera/image_raw", Image, self.callback)
 
   def callback(self,data):
@@ -23,6 +29,34 @@ class camera_1:
     _, thresh_image=cv2.threshold(gray_image,220,225,cv2.THRESH_BINARY)
     contours, _= cv2.findContours (thresh_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     for cnt in contours:
+      #getting coords from contours
+      pts= cnt.squeeze()
+      for coord in pts:
+        new=np.append(coord,[1])
+        #----------PlS TRANSFORM HERE--------------------
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        #__________________________________________________
+        #creating point cloud and publishing
+        pt_32=Point32(new[0],new[1],new[2])
+        points=[]
+        points.append(pt_32)
+        rospy.loginfo(pt_32)
+        header = std_msgs.msg.Header()
+        header.stamp = rospy.Time.now()
+        header.frame_id = 'base_link'
+        cloud=PointCloud()
+        cloud.header=header
+        cloud.points=points
+        self.cloud_pub.publish(cloud)
+
       cv2.drawContours(cv_image, [cnt],0,(255, 0, 0),3)
     cv2.imshow("Camera output", cv_image)
     cv2.imshow("Processed", thresh_image)
