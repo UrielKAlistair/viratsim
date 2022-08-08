d4eba4b4e53a3c29691998ae9fd59141aa5a0312
UKA <arvindanuk@gmail.com>
2022-08-08 16:31:13 +0530

    Publishing untrasformed potholes and configured rviz to show camera instead of the CV popup

--- scripts/potholedetector.py
+++ scripts/potholedetector.py
@@ -2,7 +2,6 @@
 
 import rospy
 
-import torch
 import numpy as np
 import cv2
 from cv_bridge import CvBridge
@@ -11,63 +10,50 @@ from sensor_msgs.msg import Image
 from sensor_msgs.msg import PointCloud
 from sensor_msgs.msg import CameraInfo
 
-from geometry_msgs.msg import Point
 from geometry_msgs.msg import Point32
 import std_msgs.msg
 
 
-def transform_and_publish(twod_coords):
+def transform_and_publish(contours):
     global cloud_pub
-    global K
     global P
 
-    if K is None or P is None:
+    if P is None:
         return
 
-    for point in twod_coords:
+    pointsin3d = []
+
+    for contour in contours:
 
         # getting coords from contours
-        pts = point.squeeze()
-        for coord in pts:
-            new = np.append(coord, [0])
-            # ----------PlS TRANSFORM HERE--------------------
-            new = np.linalg.solve(np.matmul(K, P[:, [0, 1, 3]]), new)
-            new[2] = 0
-            # __________________________________________________
-            # creating point cloud and publishing
+        cntrpts = contour.squeeze()
 
+        for point in cntrpts:
+            new = np.append(point, [1])
+            new = np.linalg.solve(P[:, [0, 1, 2]], new - P[:, 3])
             pt_32 = Point32(new[0], new[1], new[2])
-            points = [pt_32]
-            rospy.loginfo(pt_32)
-            header = std_msgs.msg.Header()
-            header.stamp = rospy.Time.now()
-            header.frame_id = 'base_link'
-            cloud = PointCloud()
-            cloud.header = header
-            cloud.points = points
-            cloud_pub.publish(cloud)
+            pointsin3d.append(pt_32)
+
+    header = std_msgs.msg.Header()
+    header.stamp = rospy.Time.now()
+    header.frame_id = 'base_link'
+    cloud = PointCloud()
+    cloud.header = header
+    cloud.points = pointsin3d
+    cloud_pub.publish(cloud)
 
 
 def camera_details_callback(data):
-    global K
     global P
 
-    Kt = data.K
     Pt = data.P
-    K = []
     P = []
-
-    for i in range(3):
-        K.append([])
-        for j in range(3):
-            K[i].append(Kt[i * 3 + j])
 
     for i in range(3):
         P.append([])
         for j in range(4):
-            P[i].append(Pt[i * 3 + j])
+            P[i].append(Pt[i * 4 + j])
 
-    K = np.array(K)
     P = np.array(P)
 
 
@@ -86,19 +72,12 @@ def img_callback(data):
     # Publishing to point cloud
     transform_and_publish(contours)
 
-    # Displaying to user
-    cv2.imshow("Camera output", og_image)  # Showing the camera feed in a new window
-    cv2.imshow("Detections", ct_image)
-    cv2.waitKey(3)
-
-
 if __name__ == '__main__':
 
     rospy.init_node('potholedetector', anonymous=False)
     image_sub = rospy.Subscriber("virat/camera/image_raw", Image, img_callback)
     cloud_pub = rospy.Publisher("point_cloud", PointCloud, queue_size=10)
 
-    K = None
     P = None
     camera_info_sub = rospy.Subscriber("virat/camera/camera_info", CameraInfo, camera_details_callback)
 
